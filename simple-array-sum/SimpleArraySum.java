8cacf7673426b6d7e3af6e25515090aa623a83a8
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-03-21 14:03:44 +0700

    simple array sum java

--- simple-array-sum/SimpleArraySum.java
+++ simple-array-sum/SimpleArraySum.java
@@ -1,5 +1,14 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 class Result {
 
@@ -13,8 +22,8 @@ class Result {
     public static int simpleArraySum(List<Integer> ar) {
         // Write your code here
         int total = 0;
-        for (int i = 0; i < ar.size(); i++) {
-            total = total + ar.get(i);
+        for (Integer value : ar) {
+            total += value;
         }
         return total;
     }
@@ -28,14 +37,9 @@ public class SimpleArraySum {
 
         int arCount = Integer.parseInt(bufferedReader.readLine().trim());
 
-        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        List<Integer> ar = new ArrayList<>();
-
-        for (int i = 0; i < arCount; i++) {
-            int arItem = Integer.parseInt(arTemp[i]);
-            ar.add(arItem);
-        }
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
 
         int result = Result.simpleArraySum(ar);
 
