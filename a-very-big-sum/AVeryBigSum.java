41a3f158cbf370b1b603b76ef0f95e3ed8fbf52b
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-04-17 08:39:23 +0700

    a very big sum java

---
+++ a-very-big-sum/AVeryBigSum.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<Long> ar) {
+        // Write your code here
+        long total = 0;
+        for (int i = 0; i < ar.size(); i++) {
+            total = total + ar.get(i);
+        }
+        return total;
+    }
+
+}
+
+public class AVeryBigSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long result = Result.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
