3de90109318d5b8625867320cce64a216cad9b24
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-04-18 20:35:04 +0700

    simple array sum

--- simple-array-sum/SimpleArraySum.java
+++ simple-array-sum/SimpleArraySum.java
@@ -1,13 +1,6 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
@@ -34,8 +27,6 @@ public class SimpleArraySum {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
                 .map(Integer::parseInt)
