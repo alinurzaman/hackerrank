783e162c58ab5b44333d5cf116a73d68fc296da6
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-04-21 22:35:33 +0700

    plus minus js

---
+++ plus-minus/plus-minus.js
@@ -0,0 +1,53 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+  // Write your code here
+  let pos = 0;
+  let neg = 0;
+  let zero = 0;
+  arr.forEach((e) => {
+    if (e > 0) pos++;
+    else if (e < 0) neg++;
+    else if (e == 0) zero++;
+  });
+  console.log(pos / arr.length);
+  console.log(neg / arr.length);
+  console.log(zero / arr.length);
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  plusMinus(arr);
+}
