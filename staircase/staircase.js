b6a8c5a9756fd47023c78651bf28f17cd25a4caf
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-04-25 18:29:48 +0700

    staircase

---
+++ staircase/staircase.js
@@ -0,0 +1,42 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function staircase(n) {
+  // Write your code here
+  let space = " ";
+  let hash = "#";
+  for (let i = 0; i < n; i++) {
+    console.log(space.repeat(n - i - 1) + hash.repeat(i + 1));
+  }
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  staircase(n);
+}
