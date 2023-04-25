111ea0cde552d5a5aa58707a2c85af9411d7b01f
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-04-25 18:39:24 +0700

    staircase

---
+++ staircase/staircase.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+func staircase(n int) {
+	// Write your code here
+	space := " "
+	hash := "#"
+	for i := 0; i < n; i++ {
+		fmt.Print(strings.Repeat(space, n-i-1))
+		fmt.Print(strings.Repeat(hash, i+1))
+		fmt.Println()
+	}
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int(nTemp)
+
+	staircase(n)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
