21f99953465bf2794c336a2c98f022494059706d
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-04-27 19:42:31 +0700

    birthday cake candles java

---
+++ birthday-cake-candles/BirthdayCakeCandles.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        // Write your code here
+        int total = 1;
+        Collections.sort(candles);
+        int tallest = candles.get(candles.size() - 1);
+        for (int i = candles.size() - 2; i >= 0; i--) {
+            if (tallest == candles.get(i))
+                total++;
+            else
+                return total;
+        }
+        return total;
+    }
+
+}
+
+public class BirthdayCakeCandles {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
