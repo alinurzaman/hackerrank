5a16fc1d828ff6916b6002ea7510b54ef474ddbd
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-04-28 20:30:46 +0700

    time conversion go

---
+++ time-conversion/time-conversion.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+func timeConversion(s string) string {
+	// Write your code here
+	hours := s[0:2]
+	minutesAndSeconds := s[2:8]
+	meridiem := s[8:10]
+
+	finalHours := hours
+	if meridiem == "PM" && hours != "12" {
+		conv, _ := strconv.ParseInt(hours, 10, 32)
+		conv += 12
+		finalHours = strconv.Itoa(int(conv))
+	} else if meridiem == "AM" && hours == "12" {
+		finalHours = "00"
+	}
+
+	return finalHours + minutesAndSeconds
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	s := readLine(reader)
+
+	result := timeConversion(s)
+
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
