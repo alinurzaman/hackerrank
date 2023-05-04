12a00909b945ef3321892184b0549793df1e01c0
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-05-04 13:30:28 +0700

    larrys array js

---
+++ larrys-array/larrys-array.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'larrysArray' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY A as parameter.
+ */
+
+function larrysArray(A) {
+  // Write your code here
+  let inversions = 0;
+  for (let i = 0; i < A.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (A[j] > A[i]) inversions++;
+    }
+  }
+  if (inversions % 2 == 0) return "YES";
+  return "NO";
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const t = parseInt(readLine().trim(), 10);
+
+  for (let tItr = 0; tItr < t; tItr++) {
+    const n = parseInt(readLine().trim(), 10);
+
+    const A = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((ATemp) => parseInt(ATemp, 10));
+
+    const result = larrysArray(A);
+
+    ws.write(result + "\n");
+  }
+
+  ws.end();
+}
