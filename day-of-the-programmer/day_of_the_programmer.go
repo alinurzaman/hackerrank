0d3aed864a63254c3f19471435057cacb02675f7
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-05-22 09:24:29 +0700

    day of the programmer go

---
+++ day-of-the-programmer/day_of_the_programmer.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+func dayOfProgrammer(year int32) string {
+	// Write your code here
+	if year == 1918 {
+		return "26.09.1918"
+	}
+
+	day := "13"
+	// gregorian calendar
+	if year > 1918 && (year%400 == 0 || (year%4 == 0 && year%100 != 0)) {
+		day = "12"
+	} else if year <= 1917 && year%4 == 0 { // julian calendar
+		day = "12"
+	}
+
+	return day + ".09." + strconv.FormatInt(int64(year), 10)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	yearTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	year := int32(yearTemp)
+
+	result := dayOfProgrammer(year)
+
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
