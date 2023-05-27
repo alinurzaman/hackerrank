f22af73e9e28ae61e76f8ea136a44465df28e59e
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-05-27 16:55:20 +0700

    drawing book js

---
+++ drawing-book/drawing-book.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+function pageCount(n, p) {
+  // Write your code here
+  if (p == 1 || p == n) return 0;
+  else if (n - 1 == p && n % 2 != 0) return 0;
+  else if (p < n - p) return Math.floor(p / 2);
+  else if (n - p == 1) return 1;
+  else return Math.floor((n - p) / 2);
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const p = parseInt(readLine().trim(), 10);
+
+  const result = pageCount(n, p);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
