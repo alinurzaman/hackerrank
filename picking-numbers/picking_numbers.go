dd7fd5fe99f63287025ae82c28ed376302795b59
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-05-31 15:18:30 +0700

    picking numbers go

---
+++ picking-numbers/picking_numbers.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+func pickingNumbers(a []int) int {
+	// Write your code here
+	sort.Ints(a)
+	max := 0
+	total := 0
+	diffOne := false
+	for i := 0; i < len(a)-1; i++ {
+		if a[i+1]-a[i] == 0 {
+			total++
+		} else if a[i+1]-a[i] == 1 && !diffOne {
+			total++
+			diffOne = true
+		} else {
+			total = 0
+			diffOne = false
+		}
+
+		if total > max {
+			max = total
+		}
+	}
+	return max + 1
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int(nTemp)
+
+	aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var a []int
+
+	for i := 0; i < int(n); i++ {
+		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+		checkError(err)
+		aItem := int(aItemTemp)
+		a = append(a, aItem)
+	}
+
+	result := pickingNumbers(a)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
