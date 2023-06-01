fad301f0c66cae0ebf01ee1f0771fb7cd05e8aea
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-01 20:17:22 +0700

    the hurdle race java

---
+++ the-hurdle-race/TheHurdleRace.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'hurdleRace' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     * 1. INTEGER k
+     * 2. INTEGER_ARRAY height
+     */
+
+    public static int hurdleRace(int k, List<Integer> height) {
+        // Write your code here
+        int max = Collections.max(height);
+        if (k >= max)
+            return 0;
+        return max - k;
+    }
+
+}
+
+public class TheHurdleRace {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> height = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.hurdleRace(k, height);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
