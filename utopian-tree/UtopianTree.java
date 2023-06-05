31704dc3fc108961fc5e70aaef4804cd0a338487
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-05 13:57:49 +0700

    utopian tree java

---
+++ utopian-tree/UtopianTree.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.stream.*;
+
+class Result {
+
+    /*
+     * Complete the 'utopianTree' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int utopianTree(int n) {
+    // Write your code here
+        int tall = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0)
+                tall++;
+            else tall *= 2;
+        }
+        return tall;
+    }
+
+}
+
+public class UtopianTree {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.utopianTree(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
