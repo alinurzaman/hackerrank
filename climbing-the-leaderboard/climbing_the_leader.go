8a90952438bedd8f36abaab954e9639c1e2b4209
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-07 20:29:46 +0700

    climbing the leader go

---
+++ climbing-the-leaderboard/climbing_the_leader.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'climbingLeaderboard' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY ranked
+ *  2. INTEGER_ARRAY player
+ */
+func removeDuplicateValues(intSlice []int32) []int32 {
+	keys := make(map[int32]bool)
+	list := []int32{}
+
+	// If the key(values of the slice) is not equal
+	// to the already present value in new slice (list)
+	// then we append it. else we jump on another element.
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+func climbingLeaderboard(ranked []int32, player []int32) []int32 {
+	// Write your code here
+	var rank []int32
+	listWithoutDuplicates := removeDuplicateValues(ranked)
+	index := len(listWithoutDuplicates) - 1
+	for i := 0; i < len(player); i++ {
+		for j := index; j >= 0; j-- {
+			if player[i] >= listWithoutDuplicates[0] {
+				rank = append(rank, 1)
+				break
+			} else if player[i] < listWithoutDuplicates[j] {
+				rank = append(rank, int32(j)+2)
+				index = j
+				break
+			} else if player[i] == listWithoutDuplicates[j] {
+				rank = append(rank, int32(j)+1)
+				index = j
+				break
+			}
+		}
+	}
+	return rank
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	rankedCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	rankedTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var ranked []int32
+
+	for i := 0; i < int(rankedCount); i++ {
+		rankedItemTemp, err := strconv.ParseInt(rankedTemp[i], 10, 64)
+		checkError(err)
+		rankedItem := int32(rankedItemTemp)
+		ranked = append(ranked, rankedItem)
+	}
+
+	playerCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	playerTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var player []int32
+
+	for i := 0; i < int(playerCount); i++ {
+		playerItemTemp, err := strconv.ParseInt(playerTemp[i], 10, 64)
+		checkError(err)
+		playerItem := int32(playerItemTemp)
+		player = append(player, playerItem)
+	}
+
+	result := climbingLeaderboard(ranked, player)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
