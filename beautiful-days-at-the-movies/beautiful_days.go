fe6a5a51becb32f987048bc21748725f5b7aeee6
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-10 10:24:08 +0700

    beautiful days go

---
+++ beautiful-days-at-the-movies/beautiful_days.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+/*
+ * Complete the 'beautifulDays' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER i
+ *  2. INTEGER j
+ *  3. INTEGER k
+ */
+func Reverse(s string) string {
+	size := len(s)
+	buf := make([]byte, size)
+	for start := 0; start < size; {
+		r, n := utf8.DecodeRuneInString(s[start:])
+		start += n
+		utf8.EncodeRune(buf[size-start:], r)
+	}
+	return string(buf)
+}
+
+func beautifulDays(i int32, j int32, k int32) int32 {
+	// Write your code here
+	var total int32 = 0
+	var l int32
+	for l = i; l <= j; l++ {
+		s := strconv.Itoa(int(l))
+		sbr := Reverse(s)
+		reversed, _ := strconv.Atoi(sbr)
+		if (int32(reversed)-l)%k == 0 {
+			total++
+		}
+	}
+	return total
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	iTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	i := int32(iTemp)
+
+	jTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	j := int32(jTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[2], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	result := beautifulDays(i, j, k)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
