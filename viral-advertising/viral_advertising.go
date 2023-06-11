fe9274b4c37c340d4d67727b0611a2c6bdb1c26e
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-11 20:21:54 +0700

    viral advertising go

---
+++ viral-advertising/viral_advertising.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'viralAdvertising' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+func viralAdvertising(n int) int {
+	// Write your code here
+	cumulative := 2
+	liked := 2
+	for i := 1; i < n; i++ {
+		liked = liked * 3 / 2
+		cumulative += liked
+	}
+	return cumulative
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int(nTemp)
+
+	result := viralAdvertising(n)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
