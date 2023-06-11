10df54c176a95fe7d9f846d26bda35863eaa7294
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-11 20:17:31 +0700

    viral advertising java

---
+++ viral-advertising/ViralAdvertising.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n) {
+    // Write your code here
+        int cumulative = 2;
+        int liked = 2;
+        for (int i = 1; i < n; i++) {
+            liked = liked * 3 / 2;
+            cumulative += liked;
+        }
+        return cumulative;
+    }
+
+}
+
+public class ViralAdvertising {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
