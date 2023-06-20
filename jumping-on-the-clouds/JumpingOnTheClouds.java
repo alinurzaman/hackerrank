071211f36bf24dceeecc951eff209448343b7da7
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-20 20:29:58 +0700

    jumping on the clouds java

---
+++ jumping-on-the-clouds/JumpingOnTheClouds.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<Integer> c) {
+    // Write your code here
+        int jumps = 0;
+        int i = 0;
+        while (i < c.size() - 1) {
+            if (i + 2 < c.size() && c.get(i + 2) == 0)
+                i += 2;
+            else
+                i++;
+            jumps++;
+        }
+        
+        return jumps;
+    }
+
+}
+
+public class JumpingOnTheClouds {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
