69595a28b18ea8ec7093f47ed821700c4dde60df
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-21 15:02:00 +0700

    camel case java

---
+++ camel-case/CamelCase.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+        // Write your code here
+        int total = 1;
+        for (Character c : s.toCharArray()) {
+            if (Character.isUpperCase(c))
+                total++;
+        }
+        return total;
+    }
+
+}
+
+public class CamelCase {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
