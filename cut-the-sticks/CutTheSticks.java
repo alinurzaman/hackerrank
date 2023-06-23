6a5ce408faf2c6429a81ee4cb0682f9f9b479ce7
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-23 08:21:19 +0700

    cut the sticks java

---
+++ cut-the-sticks/CutTheSticks.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'cutTheSticks' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> cutTheSticks(List<Integer> arr) {
+        // Write your code here
+        List<Integer> sticksCut = new ArrayList<Integer>();
+        do {
+            sticksCut.add(arr.size());
+            int min = Collections.min(arr);
+            arr = arr.stream().map(number -> number - min).filter(number -> number != 0).collect(Collectors.toList());
+        } while (arr.size() > 0);
+        return sticksCut;
+    }
+
+}
+
+public class CutTheSticks {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.cutTheSticks(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n");
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
