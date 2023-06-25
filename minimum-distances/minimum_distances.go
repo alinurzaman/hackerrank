06f6e61ad38eb006b4d4c0845aa82c4edcf46662
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-25 21:28:30 +0700

    minimum distances go

---
+++ minimum-distances/minimum_distances.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'minimumDistances' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+func minimumDistances(a []int32) int32 {
+	// Write your code here
+	var min int32 = -1
+	var value = map[int32]int32{}
+	for i := 0; i < len(a); i++ {
+		_, ok := value[a[i]]
+		if ok {
+			diff := int32(i) - value[a[i]]
+			if min == -1 || diff < min {
+				min = diff
+			}
+		} else {
+			value[a[i]] = int32(i)
+		}
+	}
+	return min
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var a []int32
+
+	for i := 0; i < int(n); i++ {
+		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+		checkError(err)
+		aItem := int32(aItemTemp)
+		a = append(a, aItem)
+	}
+
+	result := minimumDistances(a)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
