c14c0ebca1764f9dea8cc06f74cc581d1a0381ff
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-27 08:29:49 +0700

    equalize the array js

---
+++ equalize-the-array/equalize-the-array.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'equalizeArray' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function equalizeArray(arr) {
+  // Write your code here
+  let max = 0;
+  const map = new Map();
+  arr.forEach((v) => {
+    map.has(v) ? map.set(v, map.get(v) + 1) : map.set(v, 1);
+    if (map.get(v) > max) max = map.get(v);
+  });
+  return arr.length - max;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  const result = equalizeArray(arr);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
