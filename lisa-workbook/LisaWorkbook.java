31f169d642a7628c0b4fa247bf8078b37f681de0
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-06-30 17:25:08 +0700

    lisa workbook java

---
+++ lisa-workbook/LisaWorkbook.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'workbook' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY arr
+     */
+
+    public static int workbook(int n, int k, List<Integer> arr) {
+    // Write your code here
+        int special = 0;
+        int page = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= arr.get(i); j++) {
+                if (page == j)
+                    special++;
+                if (j % k == 0 || j == arr.get(i))
+                    page++;
+            }
+        }
+        return special;
+    }
+
+}
+
+public class LisaWorkbook {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.workbook(n, k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
