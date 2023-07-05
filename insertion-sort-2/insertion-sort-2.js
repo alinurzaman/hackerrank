04bbd41550fc596213ba227528675bd3ecf9622b
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-07-05 08:43:14 +0700

    insertion sort 2 js

---
+++ insertion-sort-2/insertion-sort-2.js
@@ -0,0 +1,55 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'insertionSort2' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY arr
+ */
+
+function insertionSort2(n, arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        let value = arr[i];
+        arr.splice(i, 1);
+        arr.splice(j, 0, value);
+        break;
+      }
+    }
+
+    console.log(...arr);
+  }
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  insertionSort2(n, arr);
+}
