1de92901d17f1f5ee78ba5b25154903481ff299a
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-07-07 10:40:31 +0700

    quicksort 1 java

---
+++ quicksort-1/QuickSort1.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'quickSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> quickSort(List<Integer> arr) {
+        int p = arr.get(0);
+        List<Integer> result = new ArrayList<>();
+        result.add(p);
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i) > p)
+                result.add(arr.get(i));
+            else
+                result.add(result.indexOf(p), arr.get(i));
+        }
+        return result;
+    }
+
+}
+
+public class QuickSort1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.quickSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n");
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
