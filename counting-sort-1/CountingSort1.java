3dc7df0c7dbaf477fcbe4be0c3ad26a3f8e418e8
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-07-11 13:46:21 +0700

    counting sort 1 java

---
+++ counting-sort-1/CountingSort1.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            int idx = i;
+            int count = (int) arr.stream().filter(e -> e == idx).count();
+            result.add(count);
+        }
+        return result;
+    }
+
+}
+
+public class CountingSort1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.countingSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n");
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
