21b6135a48e9385eace9176fcc8fa0071573c00d
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-07-12 14:37:35 +0700

    running time js

---
+++ running-time/running-time.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'runningTime' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function runningTime(arr) {
+  let shifts = 0;
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        shifts = shifts + i - j;
+        let value = arr[i];
+        arr.splice(i, 1);
+        arr.splice(j, 0, value);
+        break;
+      }
+    }
+  }
+  return shifts;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  const result = runningTime(arr);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
