6821b163016b11be95db4bdce5880f71358bcffa
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-07-14 20:50:12 +0700

    gem stones java

---
+++ gem-stones/GemStones.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gemstones' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING_ARRAY arr as parameter.
+     */
+
+    public static int gemstones(List<String> arr) {
+        HashSet<Character> set = new HashSet<>();
+        for (Character c : arr.get(0).toCharArray()) {
+            set.add(c);
+        }
+        List<Character> list = new ArrayList<Character>(set);
+        
+        for (String v : arr) {
+            list.removeIf(c -> !v.contains("" + c));
+        }
+        
+        return list.size();
+    }
+
+}
+
+public class GemStones {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> arr = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        int result = Result.gemstones(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
